26e3831d10d67f29aa24364b526f149c9fee0cee
guaichaAndres <56601601+guaichaAndres@users.noreply.github.com>
2021-06-06 01:09:11 -0500

    Creación de la conexión a la BD mediante EJB y JPA

---
+++ src/ec/edu/ups/ejb/ProductoFacade.java
@@ -0,0 +1,22 @@
+package ec.edu.ups.ejb;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import ec.edu.ups.modelo.Producto;
+
+@Stateless
+public class ProductoFacade extends AbstractFacade<Producto>{
+	@PersistenceContext(unitName = "FacturacionProyecto")
+    private EntityManager em;
+    
+    public ProductoFacade() {
+	super(Producto.class);
+    }    
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+}
